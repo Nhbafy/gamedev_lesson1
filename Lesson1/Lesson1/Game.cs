39fc54b3cd85e34a0f6acc79a6126c424db449d3
Nhbafy <35606966+Nhbafy@users.noreply.github.com>
2018-01-19 22:24:27 +0300

    Первый урок
    Первый урок

---
+++ Lesson1/Lesson1/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+namespace MyGame
+{
+    static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+        static Game()
+        {
+        }
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики
+            Graphics g;
+            // предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();// Создаём объект - поверхность рисования и связываем его с формой
+                                      // Запоминаем размеры формы
+            Width = form.Width;
+            Height = form.Height;
+            // Связываем буфер в памяти с графическим объектом.
+            // для того, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+            Timer timer = new Timer { Interval = 50 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+        public static void Draw()
+        {
+            // Проверяем вывод графики
+            //Buffer.Graphics.Clear(Color.Black);
+            //Buffer.Graphics.DrawRectangle(Pens.White, new Rectangle(100, 100, 200, 200));
+            //Buffer.Graphics.FillEllipse(Brushes.Wheat, new Rectangle(100, 100, 200, 200));
+           // Buffer.Render();
+
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            Buffer.Render();
+        }
+        public static Star[] _objs;
+        public static void Load()
+        {
+           
+            _objs = new Star[30];
+            for (int i = 0; i < _objs.Length; i++)
+                _objs[i] = new Star(new Point(), new Point(-3-i, 0), new Size(1, 1));
+        }        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+        }
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+    }
+}
